20a9b7b91d2a92dc82da47832b8ba55ab8bd49d6
Rain Babauta <rainbabauta1@gmail.com>
2020-08-22 18:52:43 -0700

    started setting up TestCase for state tracker class

---
+++ test_state_tracker.py
@@ -0,0 +1,11 @@
+"""StateTracker class tests"""
+
+import os
+from unittest import TestCase
+
+
+class StateTrackerTestCase(TestCase):
+	
+	def setUp(self):
+		"""Create mock environment that should be similar to running main in mainsim.py"""
+		
