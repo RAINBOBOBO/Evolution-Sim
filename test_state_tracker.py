19bcdd99a1c41c7f01785d8486d961f01d6eb584
Rain Babauta <rainbabauta1@gmail.com>
2020-08-27 17:19:08 -0700

    added a little to set up and mock functions for test_state_tracker

--- test_state_tracker.py
+++ test_state_tracker.py
@@ -2,10 +2,26 @@
 
 import os
 from unittest import TestCase
+from mainsim import StateTracker
+
+
+def mock_main():
+	pass
+
+def mock_state_handler():
+	pass
 
 
 class StateTrackerTestCase(TestCase):
 	
 	def setUp(self):
-		"""Create mock environment that should be similar to running main in mainsim.py"""
+		"""Create a StateTracker instance"""
+		state_tracker = StateTracker()
+
+
+	def test_run_once(self):
+		"""Test run_once class method"""
+
+
+
 		
