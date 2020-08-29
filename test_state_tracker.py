80ac590c65af772ebad2922e5753f79b35da6b8e
Rain Babauta <rainbabauta1@gmail.com>
2020-08-28 18:30:18 -0700

    created mock functions for menu and show matrix

--- test_state_tracker.py
+++ test_state_tracker.py
@@ -8,8 +8,23 @@ from mainsim import StateTracker
 def mock_main():
 	pass
 
-def mock_state_handler():
+def mock_menu():
+	return 2
+
+def mock_show_matrix():
 	pass
+
+def mock_state_handler(defaultState = 1):
+	done = False
+    state = defaultState
+    while done == False:
+        if state == 1:
+            state = mock_menu()
+        elif state == 2:
+            state = mock_show_matrix()
+        elif state == 3 or state == 4 or state == 5:
+            done = True
+    return state
 
 
 class StateTrackerTestCase(TestCase):
