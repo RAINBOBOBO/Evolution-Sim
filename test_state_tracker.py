184de6cd08302ad61e69e714a06ed037c3b6ae92
Rain Babauta <rainbabauta1@gmail.com>
2020-09-01 18:21:47 -0700

    ran first test (fail), removed bad file, updated .gitignore.

--- test_state_tracker.py
+++ test_state_tracker.py
@@ -13,42 +13,44 @@ def mock_main():
 		state_tracker.set_state(mock_state_handler(state_tracker.state))
 
         #if state_tracker = 3, run once, if 4, run 5 times, if 5, run 10 times
-        if state_tracker.state == 3:
-            state_tracker.run_once()
-        elif state_tracker.state == 4:
-            state_tracker.run_x_times(5)
-        elif state_tracker.state == 5:
-            state_tracker.run_x_times(10)
+		if state_tracker.state == 3:
+			state_tracker.run_once()
+		elif state_tracker.state == 4:
+			state_tracker.run_x_times(5)
+		elif state_tracker.state == 5:
+			state_tracker.run_x_times(10)
 
 def mock_menu():
 	return 2
 
 def mock_show_matrix():
-	pass
+	# next generation:
+	return 3
+	# 5 generations
+	# return 4
+	# 10 generations
+	# return 5
 
 def mock_state_handler(defaultState = 1):
 	done = False
-    state = defaultState
-    while done == False:
-        if state == 1:
-            state = mock_menu()
-        elif state == 2:
-            state = mock_show_matrix()
-        elif state == 3 or state == 4 or state == 5:
-            done = True
-    return state
+	state = defaultState
+	while done == False:
+		if state == 1:
+			state = mock_menu()
+		elif state == 2:
+			state = mock_show_matrix()
+		elif state == 3 or state == 4 or state == 5:
+			done = True
+	return state
 
-
+state_tracker = StateTracker(3)
 class StateTrackerTestCase(TestCase):
-	
-	def setUp(self):
-		"""Create a StateTracker instance"""
-		state_tracker = StateTracker()
-
-
 
 	def test_run_once(self):
 		"""Test run_once class method"""
+		print("starting run_once test")
+		mock_main()
+		print("run_once test complete")
 
 
 
